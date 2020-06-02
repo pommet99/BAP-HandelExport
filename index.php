4c8e205f998be82e9a66e843b8520d201685a3d1
ctiv-iim23 <55100662+warzix-ctivgpf1@users.noreply.github.com>
2020-06-02 18:26:18 +0200

    update home

--- index.php
+++ index.php
@@ -5,14 +5,53 @@
 <body>
 
 <div>
+    <div class="containerintro">
+        <img class="flogo" src="<?php echo get_template_directory_uri(); ?>/photos/creatv-eight-9SXZ3JnZ9Wk-unsplash.jpg" style="max-width: 100%">
+        <div class="text">
+            <h1>EQUIPEMENT DE CUISINE<br>
+            PROFESSIONNEL</h1>
+        </div>
+    </div>
 
-	<img class="flogo" src="<?php echo get_template_directory_uri(); ?>/photos/creatv-eight-9SXZ3JnZ9Wk-unsplash.jpg" style="max-width: 100%"><br>
-
-    <div class="container" style="background-color: #014077; max-width: 100%; max-height: 800px">
+    <div class="container">
+        <h1 class="firsth1">Présentation</h1>
+        <div class="rectangle"></div>
         <ul class="flex-container">
-            <li class="flex-item">1</li>
-            <li class="flex-item">2</li>
-            <li class="flex-item">3</li>
+            <li class="flex-item">
+                <h1>Qui sommes-nous ?</h1><br>
+                <div class="rectangle2"></div>
+                <p>Nous accompagnons les professionnels de la restauration collective et traditionnelle, en leur proposant le meilleur de l'équipement culinaire.</la></p>
+            </li>
+            <li class="flex-item">
+                <h1>La proximité</h1><br>
+                <div class="rectangle2"></div>
+                <p>Les établissements Handel Overseas, vous livrent, assurent le service après vente ainsi que les installations du matériel proposé.</p>
+            </li>
+            <li class="flex-item">
+                <h1>Que défendons-nous ?</h1><br>
+                <div class="rectangle2"></div>
+                <p>Toujours à votre écoute et soucieux de vous satisfaire, Handel Overseas s'engage à vous proposer du matériel de qualité.<br><br>
+                Votre métier nécessite des besoins particuliers, auxquels notre équipe commerciale vous apporte des solutions crédibles et durables.</p>
+            </li>
+        </ul>
+    </div>
+    <div class="container2">
+    <ul class="flex-container2">
+        <li class="flex-item2">
+            <div id="cercle"></div>
+        </li>
+        <li class="flex-item2">
+            <div id="cercle"></div>
+        </li>
+        <li class="flex-item2">
+            <div id="cercle"></div>
+        </li>
+    </ul>
+    </div>
+    <div class="container3">
+
+        <ul class="flex-container3">
+        <a href="http://www.adresse-de-votre-choix.com/" class="bouton2">NOS PRODUITS</a>
         </ul>
     </div>
 </div>
